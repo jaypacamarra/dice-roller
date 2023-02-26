fd9c29c5c149fb2cee1c1346be4bc99dd834802c
Jay <jpacamarra@outlook.com>
2023-02-25 20:04:54 -0800

    button dice type select

--- rtos_main/TaskLCD.ino
+++ rtos_main/TaskLCD.ino
@@ -1,3 +1,6 @@
+// create an instance of the TFT library
+TFT TFTscreen = TFT(cs, dc, rst);
+
 void TaskLCD( void *pvParameters __attribute__((unused)) )  // This is a Task.
 {
   //initialize the library
@@ -5,20 +8,14 @@ void TaskLCD( void *pvParameters __attribute__((unused)) )  // This is a Task.
   // clear the screen with a black background
   TFTscreen.background(0, 0, 0);
   //set the text size
-  TFTscreen.setTextSize(2);
+  TFTscreen.setTextSize(5);
+  // set font color
+  TFTscreen.stroke(255, 255, 255);
 
   for(;;)
-  {
-    //generate a random color
-    int redRandom = random(0, 255);
-    int greenRandom = random (0, 255);
-    int blueRandom = random (0, 255);
-    
-    // set a random font color
-    TFTscreen.stroke(redRandom, greenRandom, blueRandom);
-    
+  { 
     // print Hello, World! in the middle of the screen
-    TFTscreen.text("Hello, World!", 6, 57);
+    TFTscreen.text("17", 50, 50);
     
     // wait 200 miliseconds until change to next color
     vTaskDelay(MS_DELAY(200)); 
